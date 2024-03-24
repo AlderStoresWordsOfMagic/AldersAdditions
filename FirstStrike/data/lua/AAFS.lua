6385aa74be969b088bfb307488d87956d8cb362c
Alder <139022720+AlderStoresWordsOfMagic@users.noreply.github.com>
2024-03-24 12:58:12 -0600

    5.4 port semi-complete
    Bugfixing with the Shock Cannon (a new weapon) and a rebalance are all that are necessary before release.

--- FirstStrike/data/lua/AAFS.lua
+++ FirstStrike/data/lua/AAFS.lua
@@ -236,6 +236,29 @@ script.on_internal_event(Defines.InternalEvents.DAMAGE_AREA_HIT, function(shipMa
 end)
 
 
+
+-- [Pinpoint Shotguns - Replace burst projectile with beam.]
+mods.alder.shotgunBeams = {}
+local shotgunBeams = mods.alder.shotgunBeams
+
+shotgunBeams.AA_SHOCK_CANNON = Hyperspace.Blueprints:GetWeaponBlueprint("AA_SHOCK_CANNON_PROJECTILE")
+
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
+    local beamReplacement = shotgunBeams[weapon.blueprint.name]
+    if beamReplacement then
+        local spaceManager = Hyperspace.Global.GetInstance():GetCApp().world.space
+        local beam = spaceManager:CreateBeam(
+            beamReplacement, projectile.position, projectile.currentSpace, projectile.ownerId,
+            projectile.target, Hyperspace.Pointf(projectile.target.x, projectile.target.y + 1),
+            projectile.destinationSpace, 1, projectile.heading)
+        beam.sub_start.x = 500*math.cos(projectile.entryAngle)
+        beam.sub_start.y = 500*math.sin(projectile.entryAngle) 
+        projectile:Kill()
+    end
+end)
+
+
+
 mods.alder.sustainBeams = {}
 local sustainBeams = mods.alder.sustainBeams
 sustainBeams["AA_BEAM_SUSTAIN"] = {
