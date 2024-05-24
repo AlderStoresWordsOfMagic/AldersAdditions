a177d5328563e5334abe14268cdbff8ed91bb3e4
Alder <139022720+AlderStoresWordsOfMagic@users.noreply.github.com>
2024-05-24 16:22:30 -0600

    Internal adjustments; Pre-Energy nerfed.

--- FirstStrike/data/lua/AAFS.lua
+++ FirstStrike/data/lua/AAFS.lua
@@ -10,9 +10,9 @@ mods.alder = {}
 
 -- [Version check for Hyperspace.]
 
-if not (Hyperspace.version and Hyperspace.version.major == 1 and Hyperspace.version.minor >= 10) then
+if not (Hyperspace.version and Hyperspace.version.major == 1 and Hyperspace.version.minor >= 11) then
   if not (Hyperspace.version.patch >= 2) then
-    error("Incorrect Hyperspace version detected! Alder's Additions: First Strike requires Hyperspace 1.10.2+ to function.")
+    error("Incorrect Hyperspace version detected! Alder's Additions: First Strike requires Hyperspace 1.11.2+ to function.")
   end
 end
 
@@ -403,72 +403,6 @@ function(Projectile, preempt) --Function that runs every tick for a projectile
             dam.iDamage = sustainBeam.baseDamage --Set the iDamage field
             local pos = Projectile.shield_end --The place at which the beam projectile is terminated by the shield
             targetedShip.shieldSystem:CollisionReal(pos.x, pos.y, dam, true) --Apply damage to that point
-        end
-    end
-end)
-  
-
-
--- [Particle Shield - An energy shield that regenerates some time after taking damage.]
-
-mods.alder.particleShields = {}
-
-local particleShields = mods.alder.particleShields
-particleShields["AA_PARTICLE_SHIELD"] = {
-    max = 1,
-    regen = 1,
-    time = 10,
-    color = Graphics.GL_Color(1.0, 0.47, 0.0, 1.0)
-}
-
--- Logic
-script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
-    local shieldSys = nil
-    if pcall(function() shieldSys = ship.shieldSystem end) and shieldSys then
-        local partShieldData = nil
-        local mostShields = 0
-        for partShieldName, data in pairs(particleShields) do
-            if ship:HasAugmentation(partShieldName) ~= 0 and data.max > mostShields then
-                partShieldData = data
-                mostShields = data.max
-            end
-        end
-        if partShieldData and shieldSys.shields.power.super.first < partShieldData.max then
-            local timer = userdata_table(ship, "mods.alder.partShieldTimer")
-            if not timer.time then
-                timer.time = 0
-            end
-            timer.time = timer.time + Hyperspace.FPS.SpeedFactor/16
-            if timer.time >= partShieldData.time then
-                timer.time = 0
-                for i = 1, math.min(partShieldData.max - shieldSys.shields.power.super.first, partShieldData.regen) do
-                    shieldSys:AddSuperShield(shieldSys.center)
-                    Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("particle_shield",0.3,true)
-                end
-            end
-            timer.progress = timer.time/partShieldData.time
-            timer.color = partShieldData.color
-        end
-    end
-end)
-
--- Charge bar
-local outlineColor = Graphics.GL_Color(1.0, 1.0, 1.0, 1.0) -- Color for the outline of the charge bar (White).
-local function render_part_shield_charge(ship, width, x, y)
-    local timer = userdata_table(ship, "mods.alder.partShieldTimer")
-    local progress = timer.progress
-    if progress and progress > 0 and not ship.ship.bDestroyed then
-        Graphics.CSurface.GL_DrawRectOutline(x, y, width, 6, outlineColor, 1)
-        Graphics.CSurface.GL_DrawRect(2 + x, 2 + y, (width - 4)*progress, 2, timer.color)
-    end
-end
-script.on_render_event(Defines.RenderEvents.GUI_CONTAINER, mods.alder.doNothing, function()
-    if Hyperspace.ships.player then render_part_shield_charge(Hyperspace.ships.player, 98, 30, 89) end
-    if Hyperspace.ships.enemy then
-        if Hyperspace.Global.GetInstance():GetCApp().gui.combatControl.boss_visual then
-            render_part_shield_charge(Hyperspace.ships.enemy, 60, 767, 113)
-        else
-            render_part_shield_charge(Hyperspace.ships.enemy, 60, 892, 157)
         end
     end
 end)
