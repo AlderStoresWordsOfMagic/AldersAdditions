b424fbc7e6d6198fa98ef7a5f98efdd5028c7297
sillysandvich <teamfortress.stupid@gmail.com>
2025-05-12 14:35:37 -0400

    Change threshold for Woldo renaming

--- AldersAdditions/data/scripts/weapons/oneoffs/cum_beam_stability.lua
+++ AldersAdditions/data/scripts/weapons/oneoffs/cum_beam_stability.lua
@@ -5,7 +5,7 @@ script.on_game_event("START_BEACON", false, function() needsNameGenerated = true
 script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
     if needsNameGenerated then
         local stability = Hyperspace.playerVariables.stability
-        local shortDesc = stability > 100 and "'Woldo'" or "Cum. Beam"
+        local shortDesc = stability >= 200 and "'Woldo'" or "Cum. Beam"
         local blueprint = Hyperspace.Blueprints:GetWeaponBlueprint("AA_BEAM_CHARGEGUN_DAMAGE")
         blueprint.desc.shortTitle.data = shortDesc
         needsNameGenerated = false
